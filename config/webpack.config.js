c02987775d2892caaf86551a9998d416225acb2c
Daria Mikhailova <dariam89@mail.ru>
2016-04-21 10:50:08 +0200

    react, babel, webpack basic setup

---
+++ config/webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+console.log(__dirname);
+var BUILD_DIR = path.resolve(__dirname, '../build');
+var APP_DIR = path.resolve(__dirname, '../app');
+
+var config = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                include: APP_DIR,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
